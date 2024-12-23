89199e530b56d0b7be07214c95ff9e7e95308dfe
Abhishek Patidar <abhipatidar253@gmail.com>
2024-12-23 22:40:40 +0530

    Creating login endpoint & send AuthToken v50

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const User = require('../models/User.js');
 const router = express.Router();
 const { body, validationResult } = require('express-validator'); 
-const bcrypt = require('bcryptjs');         //but first we have to install "npm i bcryptjs" visit https://www.npmjs.com/package/bcryptjs for docs
-const jwt = require('jsonwebtoken');          //but first we have to install "npm i jsonwebtoken" visit https://www.npmjs.com/package/jsonwebtoken for docs. and visit https://jwt.io/ to understand jwt
+const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
 
 
 const JWT_SECRET = "AbhiIs$goodBoy"         //we will store it in secret file like .env.local ot ignore to push publicly
@@ -20,13 +20,13 @@ router.post('/createuser', [
     }
     try{
         // check whether the user with this email alredy exist
-        let user = await User.findOne({email: req.body.email});         //in an async function if there are promises, we have to await all the time to resolve the promises 
+        let user = await User.findOne({email: req.body.email});
         if(user){
             return res.status(400).json({error: "Sorry this email already registered"});
         }
 
         //password hashing
-        const salt = await bcrypt.genSalt(10);         //generates a salt and geSalt() is an Asynchronus funtion.
+        const salt = await bcrypt.genSalt(10); 
         secPass = await bcrypt.hash(req.body.password, salt);
         
         //creating user in the database
@@ -46,8 +46,52 @@ router.post('/createuser', [
 
     } catch(error){
         console.log(error.message);
-        res.status(500).send("Some error has occured");
+        res.status(500).send("Internal server Error");
     } 
+})
+
+//LOGIN ENDPOINT
+//Authenticate a user using POST "/api/auth/login". No login required
+
+router.post('/login', [
+    //Validations
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password can not be blank').exists()
+
+], async (req, res)=>{ 
+    //if there are errors, return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;         //array destructure syntax
+    try{
+        //checking if user with inserted email registered or not
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Incorrect Credentials"});
+        }
+        //Check if password inserted is coreect
+        const passwordCompare = await bcrypt.compare(password, user.password);      //password is the inserted one and user.password is the hashed password from the DB
+        if(!passwordCompare){
+            return res.status(400).json({error: "Incorrect Credentials"});
+        }
+
+        //if both email and password are match fetch user data with unique id
+        const data = {        
+            user:{
+                id: user.id
+            }
+        }
+        //generating and sending authtoken using jwt(json web token)
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken});
+
+    }catch(error) {
+        console.log(error.message);
+        res.status(500).send("Internal server Error");
+    }
 })
 
 module.exports = router
