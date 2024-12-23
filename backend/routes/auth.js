bf93ef0b380808f210cad207da3fbdfc2fb4acc2
Abhishek Patidar <abhipatidar253@gmail.com>
2024-12-23 15:44:48 +0530

    Password Hashing & JSON Web Token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const User = require('../models/User.js');
 const router = express.Router();
-const { body, validationResult } = require('express-validator'); //but first we have to install "npm install express-validator" visit https://express-validator.github.io/docs/guides/getting-started for docs
+const { body, validationResult } = require('express-validator'); 
+const bcrypt = require('bcryptjs');         //but first we have to install "npm i bcryptjs" visit https://www.npmjs.com/package/bcryptjs for docs
+const jwt = require('jsonwebtoken');          //but first we have to install "npm i jsonwebtoken" visit https://www.npmjs.com/package/jsonwebtoken for docs. and visit https://jwt.io/ to understand jwt
 
+
+const JWT_SECRET = "AbhiIs$goodBoy"         //we will store it in secret file like .env.local ot ignore to push publicly
 
 //Create a user using POST "/api/auth/createuser". No login required
-// router.post('/', [all validations in array], async (req, res)=>{                  
-//     console.log(req.body);                      
-//     const user = User(req.body);                
-//     user.save()                                 
-//     res.send(req.body);                         
-// })
-
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({min: 3}),
     body('email', 'Enter a valid email').isEmail(),
@@ -19,7 +16,7 @@ router.post('/createuser', [
 ], async (req, res)=>{                  
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});      //400 -> Bad request
+        return res.status(400).json({errors: errors.array()});
     }
     try{
         // check whether the user with this email alredy exist
@@ -27,12 +24,25 @@ router.post('/createuser', [
         if(user){
             return res.status(400).json({error: "Sorry this email already registered"});
         }
-        user = await User.create({               //creating user in database
+
+        //password hashing
+        const salt = await bcrypt.genSalt(10);         //generates a salt and geSalt() is an Asynchronus funtion.
+        secPass = await bcrypt.hash(req.body.password, salt);
+        
+        //creating user in the database
+        user = await User.create({               
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
-        res.json(user);
+            password: secPass,
+        });
+        
+        const data = {        
+            user:{
+                id: user.id     //we are generating authtoken using id which is there in mongoDB for uniquly idetify each user and assign authtoken
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);           //sign() is an synchronus fxn so no need of await and it take two arg.
+        res.json({authtoken});
 
     } catch(error){
         console.log(error.message);
